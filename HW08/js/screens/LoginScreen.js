5799005be9ed3a18615eb792e9bf1423472f9206
Nguyễn Chí Trung Kiên <41412023+ngchitrungkien@users.noreply.github.com>
2020-09-19 23:56:02 +0700

    Add files via upload

--- HW08/js/screens/LoginScreen.js
+++ HW08/js/screens/LoginScreen.js
@@ -2,20 +2,17 @@ import {BaseComponent} from '../BaseComponent.js'
 import {validateEmail} from '../utils.js'
 import {MD5} from '../utils.js'
 
-class RegisterScreen extends BaseComponent {
+class LoginScreen extends BaseComponent {
     constructor(){
         super();
 
         this.state= {
             errors: {
-                name: '',
                 email: '',
-                password: '',
-                confirmPassword: ''
+                password: ''
             },
 
             data: {
-                name: '',
                 email: '',
                 password: ''
             }
@@ -23,35 +20,24 @@ class RegisterScreen extends BaseComponent {
     }
     render(){
         this._shadowRoot.innerHTML = /* html */ `
-        <section class='register-screen'>
-            <form class='form-register'>
-                <input-wrapper class='name' label='Name' type='text' error='${this.state.errors.name}' value='${this.state.data.name}'></input-wrapper>
+        <section class='login-screen'>
+            <form class='form-login'>
                 <input-wrapper class='email' label='Email' type='email' error='${this.state.errors.email}' value='${this.state.data.email}'></input-wrapper>
                 <input-wrapper class='password' label='Password' type='password' error='${this.state.errors.password}' value='${this.state.data.password}'></input-wrapper>
-                <input-wrapper class='confirm-password' label='Confirm password' type='password' error='${this.state.errors.confirmPassword}'></input-wrapper>
-                <button class='btn-register'>Register</button>
+                <button class='btn-login'>Login</button>
             </form>
         </section>
         `;
 
-        this.$formRegister = this._shadowRoot.querySelector('.form-register');
-        this.$formRegister.onsubmit = async (event) => { 
+        this.$formLogin = this._shadowRoot.querySelector('.form-login');
+        this.$formLogin.onsubmit = async (event) => { 
             event.preventDefault();
             // Lấy dữ liệu từ các input-wrapper
-            let name = this._shadowRoot.querySelector('.name').value;
             let email = this._shadowRoot.querySelector('.email').value;
             let password = this._shadowRoot.querySelector('.password').value;
-            let confirmPassword = this._shadowRoot.querySelector('.confirm-password').value;
 
             // Kiểm tra dữ liệu nhập vào, nếu có lỗi thì show ra
             let isPassed = true;
-
-            if (name == '') {
-                this.state.errors.name = 'Input your name!';
-            } else {
-                this.state.errors.name = '';
-                this.state.data.name = name;
-            }
 
             if (email == '' || !validateEmail(email)) {
                 this.state.errors.email = 'Input your email!';
@@ -68,30 +54,19 @@ class RegisterScreen extends BaseComponent {
                 this.state.data.password = password;
             }
 
-            if (confirmPassword == '' || confirmPassword != password) {
-                isPassed = false;
-                this.state.errors.confirmPassword = 'Your password is wrong!';
-            } else {
-                this.state.errors.confirmPassword = '';
-            }
-            
             // Lưu dữ liệu vào firebase
-            if(isPassed) {
-                this.state.data.password = MD5(this.state.data.password).toString();
-                // check email trùng
+            if (isPassed) {
                 let response = await firebase.firestore()
                     .collection('users')
-                    .where('email','==', email)
+                    .where('email', '==', email)
+                    .where('password', '==', MD5(password))
                     .get();
 
-                // thêm 
                 if (response.empty) {
-                    await firebase.firestore().collection('users').add(this.state.data);
-                    alert('Sign up successfully!');
+                    alert('Your email or password is not correct!')
                 } else {
-                    alert('Your email has already been used!');
+                    alert('Sign in successfully!')
                 }
-                
             }
 
             this.setState(this.state);
@@ -99,4 +74,4 @@ class RegisterScreen extends BaseComponent {
     }
 }
 
-window.customElements.define('register-screen', RegisterScreen)
+window.customElements.define('login-screen', LoginScreen)
